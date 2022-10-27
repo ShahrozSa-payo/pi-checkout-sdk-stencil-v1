46d5557672ea43adc87a2b7b642d01bb527d4b81
Shahroz Saleem <shahrozsa@payoneer.com>
2022-10-27 11:02:49 +0500

    initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'pi-checkout-sdk-stencil-v1',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
