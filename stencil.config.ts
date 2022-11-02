37b1683a38ed46326ad7e62a80a6ea7fb1eb6941
Shahroz Saleem <shahrozsa@payoneer.com>
2022-11-02 17:17:43 +0500

    added extras in build config

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,16 @@ import { Config } from '@stencil/core';
 export const config: Config = {
   namespace: 'pi-checkout-sdk-stencil-v1',
   buildEs5: true,
+  extras: {
+    cssVarsShim: true,
+    dynamicImportShim: true,
+    shadowDomShim: true,
+    safari10: true,
+    scriptDataOpts: true,
+    appendChildSlotFix: false,
+    cloneNodeFix: false,
+    slotChildNodesFix: true,
+  },
   outputTargets: [
     {
       type: 'dist',
